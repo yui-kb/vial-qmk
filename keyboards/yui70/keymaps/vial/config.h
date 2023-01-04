bf8eb4098047cfec7ae0d897609c8a22bc6b847c
Yui <middlebrook.george@gmail.com>
2023-01-04 01:04:51 +0000

    Pls fix <3

---
+++ keyboards/yui70/keymaps/vial/config.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x0F, 0x0A, 0x2C, 0xFD, 0xF0, 0x08, 0x05, 0x6B}
+
+#define VIAL_UNLOCK_COMBO_ROWS {0, 7}
+#define VIAL_UNLOCK_COMBO_COLS {0, 10}
